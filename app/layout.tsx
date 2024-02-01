328fa936c74b1d929580219f2e6fbd31561a4548
Fábio Luiz Nery de Miranda <fabio@7gen.com>
2024-02-01 14:25:14 -0800

    Set the Roboto font as it's preferred for use with MUI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Roboto } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+})
+
+import "./globals.css";
 
 export default function RootLayout({
   children,
@@ -10,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         {children}
       </body>
     </html>
